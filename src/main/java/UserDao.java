a1334837740c9a51b28a52493686c798eacc8536
heechang.kwon <facee22av@naver.com>
2021-06-23 08:39:30 +0900

    [Modify] 스프링 JdbcTemplate 적용

--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -1,6 +1,8 @@
 import Strategy.AddStatement;
 import Strategy.DeleteAllStatement;
 import Strategy.StatementStrategy;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -9,37 +11,39 @@ public class UserDao {
 
     private DataSource dataSource;
     private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
     public void setJdbcContext(JdbcContext jdbcContext) {
         this.jdbcContext = jdbcContext;
     }
 
     public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
         this.dataSource = dataSource;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException{
-        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
-                return ps;
-            }
-        });
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException{
-        Connection c = dataSource.getConnection();
-        ///
-        return null;
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
+                new Object[] {id},
+                new RowMapper<User>(){
+                    public User mapRow(ResultSet rs, int rowNum)
+                            throws SQLException{
+                        User user = new User();
+                        user.setId(rs.getString("id"));
+                        user.setName(rs.getString("name"));
+                        user.setPassword("password");
+                        return user;
+                    }
+                });
+
     }
 
     public void deleteAll() throws SQLException{
-        this.jdbcContext.executeSql("delete from users");
+        this.jdbcTemplate.update("delete from user_defined_type_schema; ");
     }
 
 
@@ -63,18 +67,6 @@ public class UserDao {
     }*/
 
     public int getCount() throws SQLException{
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
-
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return count;
+        return this.jdbcTemplate.queryForInt("select count(*) from users");
     }
 }
