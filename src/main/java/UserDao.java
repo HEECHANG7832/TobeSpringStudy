ff1e1b3f79a771315d5237827cb9686c233d322c
heechang.kwon <facee22av@naver.com>
2021-06-23 07:54:58 +0900

    [Modify] Template, 전략패턴 구현

--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -8,26 +8,46 @@ import java.sql.*;
 public class UserDao {
 
     private DataSource dataSource;
+    private JdbcContext jdbcContext;
+
+    public void setJdbcContext(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
+    }
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException{
-        StatementStrategy st = new AddStatement(user);
-        jdbcContextWithStatementStratege(st);
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+                return ps;
+            }
+        });
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException{
         Connection c = dataSource.getConnection();
+        ///
+        return null;
     }
 
     public void deleteAll() throws SQLException{
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStratege(st);
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                return c.prepareStatement("delete from users");
+            }
+        });
     }
 
-    public void jdbcContextWithStatementStratege(StatementStrategy stmt) throws SQLException{
+    /*public void jdbcContextWithStatementStratege(StatementStrategy stmt) throws SQLException{
         Connection c = null;
         PreparedStatement ps = null;
 
@@ -44,7 +64,7 @@ public class UserDao {
             if (c != null) {try {c.close();} catch (SQLException e){}}
         }
 
-    }
+    }*/
 
     public int getCount() throws SQLException{
         Connection c = dataSource.getConnection();
