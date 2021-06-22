c4616a6a921ec2c0d36bb7ddfac1b1c9485cfa18
heechang.kwon <facee22av@naver.com>
2021-06-23 07:59:14 +0900

    [Modify] 템플릿/콜백 구현

--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -39,13 +39,9 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException{
-        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                return c.prepareStatement("delete from users");
-            }
-        });
+        this.jdbcContext.executeSql("delete from users");
     }
+
 
     /*public void jdbcContextWithStatementStratege(StatementStrategy stmt) throws SQLException{
         Connection c = null;
